5b88143917dc6e906692b7b46c6bc1d3b0662de3
arckmr <54217498+arckmr@users.noreply.github.com>
2021-05-01 04:00:43 +0530

    finished auth

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleclientID:"802959246310-9287ecnp9rf694hla7q21pjpa5s4ln12.apps.googleusercontent.com",
+  googleclientSecret: "CC18zVhMPYkSB-728_IS5nYW",
+  mongoURI: 'mongodb+srv://archit:archit@mern.lpolz.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'dsvdfbvsxdbdfbdfbvdfvdvfvdfv'
+  //SRn1vKQN7vkWkswW
+};
---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleclientID:process.env.GOOGLE_CLIENT_ID,
+  googleclientSecret:process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey:process.env.COOKIE_KEY
+  //SRn1vKQN7vkWkswW
+};
