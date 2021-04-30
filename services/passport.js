48b5fdd3069b05a8b027937bbcc3769c15b0ccee
arckmr <54217498+arckmr@users.noreply.github.com>
2021-05-01 04:11:30 +0530

    finished auth

--- services/passport.js
+++ services/passport.js
@@ -24,6 +24,7 @@ passport.use(
   clientID: keys.googleclientID,
   clientSecret: keys.googleclientSecret,
   callbackURL: '/auth/google/callback',
+  proxy: true
 },
 
   (accessToken, refreshToken, profile, done) =>{
